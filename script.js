d31d015f917beb81d8171a11b3b59b071b474e39
Emerson Martins <emerson.garcao@gmail.com>
2023-10-24 16:31:02 -0300

    Criação de funções adicionar, apagar e limpar dados

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function criaTarefa(textoInput){
   const li = criaLi();
   li.innerText = textoInput;
   tarefas.appendChild(li);
+  limpaInput();
+  criaBtnApagar(li);
 }
 btnTarefa.addEventListener("click", function(e){
   if(!inputTarefa.value) return;
@@ -19,7 +21,27 @@ btnTarefa.addEventListener("click", function(e){
 })
 //Ativar Função "Enter"
 inputTarefa.addEventListener("keypress", function(e){
-  if (e.keyCode === 13){
+    if (e.keyCode === 13){
     criaTarefa(inputTarefa.value)
+  }
+})
+//Limpar input
+function limpaInput(){
+  inputTarefa.value = "";
+  inputTarefa.focus();
+}
+//Botão Apagar
+function criaBtnApagar(li){
+const botaoApagar = document.createElement('button');
+botaoApagar.innerText = "Apagar";
+botaoApagar.setAttribute("class", "apagar");
+botaoApagar.setAttribute("title", "Apagar esta tarefa");
+li.appendChild(botaoApagar);
+}
+document.addEventListener('click', function(e){
+  const el = e.target;
+  if(el.classList.contains('apagar')){
+
+    el.parentElement.remove()
   }
 })
